8b0c133bc20555d1958f5d0403a2b94aa4f8a23d
ulugbek <ulugbekyusupov272@gmail.com>
2024-02-26 17:08:41 +0500

    singleton

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,9 +1,12 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    private static ThreadSafeSingleton instance = new ThreadSafeSingleton();
+    private static ThreadSafeSingleton instance;
 
     public static ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            instance = new ThreadSafeSingleton();
+        }
         return instance;
     }
 }
